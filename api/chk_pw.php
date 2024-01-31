6204ecde67fd9fd7db73c265057cd8fde05ed4b9
Angie <liya0706s@gmail.com>
2024-01-31 08:53:49 +0800

    會員登入時看的到返回管理????

--- api/chk_pw.php
+++ api/chk_pw.php
@@ -12,6 +12,7 @@ $chk=$db->count($_POST);
 // php有值除了0, 其他都是true
 if($chk){
     echo $chk;
+    // table 是 管理者admin 或是會員mem
     $_SESSION[$table]=$_POST['acc'];
     // 如果 $table 是 "users"，則用戶帳號將被保存在 $_SESSION['users'] 中。
     // 这使得在整个会话期间可以跟踪用户的登录状态，而无需重复查询数据库。
--- index.php
+++ index.php
@@ -23,11 +23,33 @@
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
                 <a href="?do=buycart">購物車</a> |
-                <a href="?do=login">會員登入</a> |
-                <a href="?do=admin">管理登入</a>
+
+                <?php
+                // 如果有是使用者
+                if (isset($_SESSION['mem'])) {
+                ?>
+                    <a href="./api/logout.php">登出</a> |
+                <?php
+                } else {
+                ?>
+                    <a href="?do=login">會員登入</a> |
+                <?php
+                }
+                ?>
+                <?php 
+                if (isset($_SESSION['admin'])) {
+                ?>
+                    <a href="back.php">返回管理</a>
+                <?php
+                } else {
+                ?>
+                    <a href="?do=admin">管理登入</a>
+                <?php
+                }
+                ?>
             </div>
             <marquee>
-                年終特賣會開跑了 &nbsp;  情人節特惠活動 &nbsp;
+                年終特賣會開跑了 &nbsp; 情人節特惠活動 &nbsp;
             </marquee>
         </div>
         <div id="left" class="ct">
@@ -40,7 +62,7 @@
             </span>
         </div>
         <div id="right">
-            
+
             <?php
             $do = $_GET['do'] ?? 'main';
             $file = "./front/{$do}.php";
@@ -54,7 +76,7 @@
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
             <?= $Bottom->find(1)['bottom']; ?></div>
     </div>
-<script src="./js/jquery-3.4.1.min.js"></script>
+    <script src="./js/jquery-3.4.1.min.js"></script>
 </body>
 
 </html>
