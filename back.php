991998f640fef63f4234a7b737e90590dfe41e26
Angie <liya0706s@gmail.com>
2024-01-31 10:31:22 +0800

    將del()搬到js.js,同時調整jquery引入在head

--- back.php
+++ back.php
@@ -9,6 +9,7 @@
 
 	<title>┌精品電子商務網站」</title>
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
+	<script src="./js/jquery-3.4.1.min.js"></script>
 	<script src="./js/js.js"></script>
 </head>
 
@@ -46,9 +47,6 @@
 		<div id="bottom" style="line-height:70px; color:#FFF; background:url(icon/bot.png);" class="ct">
 			<?= $Bottom->find(1)['bottom']; ?></div>
 	</div>
-
-	<script src="./js/jquery-3.4.1.min.js"></script>
-
 </body>
 
 </html>
--- js/js.js
+++ js/js.js
@@ -3,3 +3,11 @@ function lof(x)
 {
 	location.href=x
 }
+
+// 外部引入，不同頁面間都會用到del function
+// 刪除的函數
+function del(table,id){
+	$.post("./api/del.php",{table,id},()=>{
+		location.reload();
+	})
+} 
