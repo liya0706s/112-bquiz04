3cfe24fb7dfecf20b79367700151eb21fdaceaa9
liya0706s <liya0706s@gmail.com>
2024-02-16 11:56:38 +0800

    在首頁取得不同分類的商品資料 main抓商品文字

--- front/main.php
+++ front/main.php
@@ -5,22 +5,33 @@
 // $type=$_GET['type'];
 // }
 
-$type=$_GET['type']??0;
-$nav='';
+$type = $_GET['type'] ?? 0;
+$nav = '';
+$goods = null;
 
-if($type==0){
-$nav="全部商品";
-}else{
-    $t=$Type->find($type);
+if ($type == 0) {
+    $nav = "全部商品";
+    $goods=$Goods->all(['sh'=>1]);
+} else {
+    $t = $Type->find($type);
     // 分類大分類還是中分類
-    if($t['big_id']==0){
-        $nav=$t['name'];
-    }else{
-        $b=$Type->find($t['big_id']);
-        $nav=$b['name'] ." > ". $t['name'];
+    if ($t['big_id'] == 0) {
+        $nav = $t['name'];
+        // 把所有商品撈出來
+        $goods = $Goods->all(['sh' => 1, 'big' => $t['id']]);
+    } else {
+        $b = $Type->find($t['big_id']);
+        $nav = $b['name'] . " > " . $t['name'];
+        $goods = $Goods->all(['sh' => 1, 'mid' => $t['id']]);
     }
 }
 
 ?>
 
-<h2><?=$nav;?></h2>
+<h2><?= $nav; ?></h2>
+<?php
+foreach ($goods as $good) {
+    echo $good['name'];
+    echo "<br>";
+}
+?>
